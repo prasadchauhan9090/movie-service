d8bc77982f3ab0c08d666628dd7a170cf61c49a1
prasad chauhan <67370566+prasadchauhan9090@users.noreply.github.com>
2026-06-18 23:23:26 +0530

    Implement global exception handling for API

--- src/main/java/com/chauhan/movie_service/api/GlobalExceptionHandler.java
+++ src/main/java/com/chauhan/movie_service/api/GlobalExceptionHandler.java
@@ -1,4 +1,65 @@
 package com.chauhan.movie_service.api;
 
+import com.chauhan.movie_service.exception.InvalidDataException;
+import com.chauhan.movie_service.exception.NotFoundException;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ExceptionDepthComparator;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+@Slf4j
 public class GlobalExceptionHandler {
+
+    @Getter
+    static  class Error{
+
+        private final String reason;
+        private final String message;
+
+
+        Error(String reason, String message) {
+            this.reason = reason;
+            this.message = message;
+        }
+    }
+
+    //400 error bad request
+    @ExceptionHandler({InvalidDataException.class, HttpMessageNotReadableException.class})
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Error handleInvalidDataException(Throwable t)
+    {
+        log.warn(t.getMessage());
+        return new Error(HttpStatus.BAD_REQUEST.getReasonPhrase(), t.getMessage());
+
+    }
+
+    //404 error bad request
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Error handleNotFoundException(NotFoundException ex)
+    {
+        log.warn(ex.getMessage());
+        return new Error(HttpStatus.BAD_REQUEST.getReasonPhrase(), ex.getMessage());
+    }
+
+    //500 error bad request
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Error handleUnknownException(Exception ex)
+    {
+        log.error(ex.getMessage());
+        return new Error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), ex.getMessage());
+    }
+
+
+
 }
